6e0be6bbf86f1dcb983619e944e9a30060f518e4
mdube123 <manasndubebpl@gmail.com>
2024-09-14 13:25:18 +0530

    remove-all

--- backend/backend-django/api1/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Backend1AppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'backend1app'
--- backend/backend-django/api1/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-rom django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('', views.home, name='home'),
-]
